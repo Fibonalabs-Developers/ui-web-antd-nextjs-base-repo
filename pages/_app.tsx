479d6fd2e3d600ca3b0d6875a387ab037204a9b3
sehalsein <sehalsein@pop-os.localdomain>
2021-07-16 17:59:08 +0530

    Ant Design Template Init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>Fibonalabs</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
-export default MyApp
