a452026d7a4908ecd4b0f4bb62caa61f4441867c
aayush <aayush.jhunjhunwala@fibonalabs.com>
2021-07-27 11:03:58 +0530

    feat: 🎸 Added Feature flags
    Implemented Feature Flag for the projects and added the documentation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import { useDummyList } from '@/src/api/demo'
 import useTranslation from 'next-translate/useTranslation'
-
+import { hasPermissions, PermissionTypes } from '@/src/utils/hasPermissions'
 const Home = () => {
     const { t } = useTranslation()
     const { data } = useDummyList()
+
+    const features: Record<number, PermissionTypes> = {
+        '1': {
+            name: 'login',
+            canEdit: true,
+            canCreate: true,
+            display: false,
+        },
+        '2': {
+            name: 'adminDashboard',
+            canEdit: true,
+            canCreate: true,
+            display: true,
+        },
+        '101': {
+            name: 'Signup',
+            canEdit: true,
+            canCreate: true,
+            display: true,
+        },
+    }
 
     return (
         <>
@@ -13,6 +34,10 @@ const Home = () => {
                     <li key={id}>{title}</li>
                 ))}
             </ul>
+
+            {hasPermissions(features, { moduleId: 2, key: 'display' }) ? (
+                <button> login</button>
+            ) : null}
         </>
     )
 }
